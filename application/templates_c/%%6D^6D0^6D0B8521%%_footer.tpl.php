afc4ef09beae6582c56e7130e80a0324ac3e1fe9
root <root@ip-172-31-19-163.eu-central-1.compute.internal>
2016-05-06 03:05:25 +0000

    Change design

--- application/templates_c/%%6D^6D0^6D0B8521%%_footer.tpl.php
+++ application/templates_c/%%6D^6D0^6D0B8521%%_footer.tpl.php
@@ -1,1 +1,1 @@
-<?php /* Smarty version 2.6.27, created on 2016-05-05 06:44:03 compiled from _footer.tpl */ ?></body></html>
+<?php /* Smarty version 2.6.27, created on 2016-05-06 02:55:19 compiled from _footer.tpl */ ?></body></html>
--- application/templates_c/%%A5^A58^A58E4C85%%patternsShow.tpl.php
+++ application/templates_c/%%A5^A58^A58E4C85%%patternsShow.tpl.php
@@ -1,2 +1,2 @@
-<?php /* Smarty version 2.6.27, created on 2016-05-04 03:34:24 compiled from modules/patterns/patternsShow.tpl */ ?>
+<?php /* Smarty version 2.6.27, created on 2016-05-05 06:33:06 compiled from modules/patterns/patternsShow.tpl */ ?>
 <?php echo $this->_tpl_vars['obj']['playlist']; ?>
